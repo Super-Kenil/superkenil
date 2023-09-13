7ac5c8597018caf3a5694b0067a7d064faf23721
Super-Kenil <kenilsudani.blog@gmail.com>
2023-09-13 23:15:37 +0530

    WIP: landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
 
+import myImg from "@/assets/images/mine.webp"
+
 const Home = () => {
   return (
-    <main className="flex bg-[#010101] container min-h-screen">
-      <section className="p-16">
-        <h1 className="text-6xl text-red-600">Super Kenil</h1>
+    <main className="flex bg-[#010101] min-h-screen text-white">
+      <section
+        className=" flex flex-col justify-between w-full bg-center bg-no-repeat"
+        // bg-[url('@/assets/images/mine.jpg')] 
+      style={{ backgroundImage: 'url(' + myImg + ')' }}
+      >
+        <header className="p-16 uppercase font-bold text-sm">Portfolio</header>
+        <div>
+
+          <div className="flex justify-between text-[170px] font-serif font-thin overflow-hidden">
+            <p className="lowercase -translate-x-24">super</p>
+            <p className="lowercase translate-x-20">kenil</p>
+          </div>
+
+        </div>
+        <nav className="p-16 flex justify-between">
+          <span>&copy; {new Date().getFullYear()}</span>
+          <ul className="flex gap-28">
+            <li className="uppercase">Preview</li>
+            <li className="uppercase">Portfolio</li>
+            <li className="uppercase">Github</li>
+          </ul>
+          <span></span>
+        </nav>
       </section>
     </main>
   )
